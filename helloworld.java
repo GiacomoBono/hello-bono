ac479ef8c6fd01c86bce5d2c36e06bc1b5ac919f
bono.giacomo08 <bono.giacomo08@gmail.com>
2026-05-24 07:51:21 +0000

    Stampa dell'anno di nascita

--- helloworld.java
+++ helloworld.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class helloworld{
     public static void main(String[] args) {
         System.out.println("Hello Giacomo Bono!");
-        System.out.println("Hello Giacomo Bono!");
-        System.out.println("Hello Giacomo Bono!");
+        System.out.println("01/07/2008");
     }
 }
