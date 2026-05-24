63a66bdd75f81b98cfd76a769cc73c7ca6b930c7
bono.giacomo08 <bono.giacomo08@gmail.com>
2026-05-24 07:53:08 +0000

    Stampa della squadra del cuore

--- helloworld.java
+++ helloworld.java
@@ -4,5 +4,6 @@ public class helloworld{
     public static void main(String[] args) {
         System.out.println("Hello Giacomo Bono!");
         System.out.println("01/07/2008");
+        System.out.println("FORZA DORIA!!");
     }
 }
