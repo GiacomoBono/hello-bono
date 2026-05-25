818d1ab0990e50aa1237d3689b8ac57520fe53fc
bono.giacomo08 <bono.giacomo08@gmail.com>
2026-05-25 11:42:22 +0000

    Aggiunta stampa Vector

--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,20 @@
-import java.util.*;
+import java.util.Vector;
 
 public class helloworld{
     public static void main(String[] args) {
         System.out.println("Hello Giacomo Bono!");
-        System.out.println("01/07/2008");
-        System.out.println("FORZA DORIA!!");
+        
+        Vector<Character> lettere = new Vector<>();
+        lettere.add('g');
+        lettere.add('i');
+        lettere.add('a');
+        lettere.add('c');
+        lettere.add('o');
+        lettere.add('m');
+        lettere.add('o');
+
+        for(Character lettera : lettere){
+            System.out.println(lettera);
+        }
     }
 }
